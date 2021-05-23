05894634d12fcc58e5130499424b3e490323a9e5
mrzacq <hartantoaanan@gmail.com>
2021-05-23 09:47:06 +0700

    newline

--- tilt-card-3d/index.js
+++ tilt-card-3d/index.js
@@ -1,18 +1,20 @@
-const box = document.querySelector('.container')
-const boxRect = box.getBoundingClientRect()
-const circle = document.querySelector('.circle')
-box.addEventListener('mousemove', e => {
-    const xPosition = (e.clientX - boxRect.left) / boxRect.width
-    const yPosition = (e.clientY - boxRect.top) / boxRect.height - 0.6
-    const xOffset = -(xPosition - 0.6)
-    const dxNorm = Math.min(Math.max(xOffset, -0.6), 0.6)
-    box.style.transform = `perspective(1000px) rotateY(${dxNorm*45}deg) rotateX(${yPosition*45}deg)`
-})
+const box = document.querySelector(".container");
+const boxRect = box.getBoundingClientRect();
+const circle = document.querySelector(".circle");
+box.addEventListener("mousemove", (e) => {
+  const xPosition = (e.clientX - boxRect.left) / boxRect.width;
+  const yPosition = (e.clientY - boxRect.top) / boxRect.height - 0.6;
+  const xOffset = -(xPosition - 0.6);
+  const dxNorm = Math.min(Math.max(xOffset, -0.6), 0.6);
+  box.style.transform = `perspective(1000px) rotateY(${
+    dxNorm * 45
+  }deg) rotateX(${yPosition * 45}deg)`;
+});
 
-box.addEventListener('mouseleave', () => {
-    box.style.transform = 'none'
-}) 
+box.addEventListener("mouseleave", () => {
+  box.style.transform = "none";
+});
 
-circle.addEventListener('click', () => {
-    alert('Hello world')
-})
+circle.addEventListener("click", () => {
+  alert("Hello world");
+});
